5972a2fe9fd2c37aed4917a7220389d240a590d3
Muhammad alfarizi <alfakiddrock7@gmail.com>
2026-02-16 11:21:16 +0800

    update hero section new

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,33 +1,37 @@
     <div class="relative h-auto bg-white">
-        <div class="flex justify-between items-center border-b w-full h-11.75 px-17.5">
-            <span class="text-sm font-bold font-sans text-black">Thinking in systems. Writing in code.</span>
-            <div class="flex gap-5">
-                <img src="/icons/ig.svg" alt="" class="h-4 w-4">
-                <img src="/icons/fb.svg" alt="" class="h-4 w-4">
-                <img src="/icons/x.svg" alt="" class="h-4 w-4">
-                <img src="/icons/pt.svg" alt="" class="h-4 w-4">
+        <div class="flex justify-between items-center border-b w-full h-11.75 lg:px-17.5 md:px-8 px-4">
+            <span class="text-sm max-md:text-xs font-bold font-sans text-black">Thinking in systems. Writing in code.</span>
+            <div class="flex gap-5 max-md:gap-3">
+                <img src="/icons/ig.svg" alt="" class="h-4 w-4 max-md:h-3 max-md:w-3">
+                <img src="/icons/fb.svg" alt="" class="h-4 w-4 max-md:h-3 max-md:w-3">
+                <img src="/icons/x.svg" alt="" class="h-4 w-4 max-md:h-3 max-md:w-3">
+                <img src="/icons/pt.svg" alt="" class="h-4 w-4 max-md:h-3 max-md:w-3">
             </div>
         </div>
     </div>
 
 
-    <div class="sticky top-0 z-10 bg-white w-full shadow-md">
-        <div class="flex h-25 justify-between items-center w-full px-17.5">
+    <div class="sticky top-0 z-10 bg-white w-full shadow-md max-sm:hidden">
+        <div class="flex h-25 justify-between items-center w-full lg:px-17.5 md:px-8">
             {{-- logo --}}
             <div class="flex w-auto justify-start items-center">
-                <img src="/icons/logo.svg" alt="" class="h-15 w-15">
-                <h1 class="text-3xl font-serif font-black">Stack<span class="text-blue-500">Root</span></h1>
+                <img src="/icons/logo.svg" alt="" class="h-15 w-15 max-lg:h-13 max-lg:w-13 ">
+                <h1 class="text-3xl max-lg:text-2xl font-serif font-black">Stack<span class="text-blue-500">Root</span></h1>
             </div>
-
             {{-- nav --}}
             <div class="flex justify-between items-center">
                 <div class="flex">
-                    <nav class="flex gap-12 border-r px-6">
-                        <a href="#" class="text-lg font-bold text-black hover:text-blue-600">Home</a>
-                        <a href="#" class="text-lg font-bold text-black hover:text-blue-600">Articles</a>
-                        <a href="#" class="text-lg font-bold text-black hover:text-blue-600">Tutorials</a>
-                        <a href="#" class="text-lg font-bold text-black hover:text-blue-600">About</a>
-                        <a href="#" class="text-lg font-bold text-black hover:text-blue-600">Contact</a>
+                    <nav class="flex gap-12 max-lg:gap-6 border-r px-6">
+                        <a href="#"
+                            class="text-lg max-lg:text-sm font-bold text-black hover:text-blue-600">Home</a>
+                        <a href="#"
+                            class="text-lg max-lg:text-sm font-bold text-black hover:text-blue-600">Articles</a>
+                        <a href="#"
+                            class="text-lg max-lg:text-sm font-bold text-black hover:text-blue-600">Tutorials</a>
+                        <a href="#"
+                            class="text-lg max-lg:text-sm font-bold text-black hover:text-blue-600">About</a>
+                        <a href="#"
+                            class="text-lg max-lg:text-sm font-bold text-black hover:text-blue-600">Contact</a>
                     </nav>
                 </div>
 
@@ -36,7 +40,29 @@
                 </div>
             </div>
         </div>
-
     </div>
 
 
+    <div class="sticky top-0 z-10 bg-white w-full shadow-md sm:hidden">
+        <div class="flex h-20 justify-between items-center w-full px-4">
+            {{-- logo --}}
+            <div class="flex w-auto justify-start items-center">
+                <img src="/icons/logo.svg" alt="" class="h-15 w-15 max-sm:h-10 max-sm:w-10">
+                <h1 class="text-3xl max-sm:text-[20px] font-serif font-black">Stack<span class="text-blue-500">Root</span></h1>
+            </div>
+
+            <div class="flex h-6 w-6 justify-center items-center">
+                <div class="background">
+                <button class="menu__icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
+
+            </div>
+
+            
+
+        </div>
+    </div>
--- resources/views/partials/hero-section.blade.php
+++ resources/views/partials/hero-section.blade.php
@@ -1,122 +1,63 @@
-<section class=" w-full">
-    <div class="relative grid grid-cols-5 grid-rows-5 gap-4 h-125 justify-between bg-gray-200 ">
-        {{-- <img src="/images/hero.png" alt="Hero Image" class="w-full h-full object-cover ">
-        <div class="absolute inset-0 bg-black/50 backdrop-blur-xs"></div> --}}
-        <div class="col-span-3 row-span-5 justify-center bg-white backdrop-blur-md px-6 py-6 mx-6 my-7 rounded-xl shadow-xl gap-10">
-
-            <!-- Top content -->
-            <div>
-                <!-- Category -->
-                <span class="text-sm font-semibold text-blue-600 uppercase tracking-wide">
-                    Laravel
-                </span>
-
-                <!-- Title -->
-                <h2 class="mt-2 text-3xl font-serif font-bold text-gray-900 leading-snug">
-                    Cara Membuat REST API di Laravel 11
-                </h2>
-
-                <!-- Description -->
-                <p class="mt-3 text-gray-600 text-sm leading-relaxed">
-                    Panduan lengkap membangun REST API modern menggunakan Laravel
-                    dengan best practice dan struktur clean architecture.
-                </p>
-            </div>
-
-            <!-- Bottom content -->
-            <div class="flex items-center justify-between text-xs text-gray-500">
-                <span class="italic">By Alpha</span>
-                <span class="font-serif">14 Feb 2026</span>
-            </div>
-
-        </div>
-
-        <div class="col-span-2 row-span-5 col-start-4 bg-white mr-6 my-7 rounded-sm ">
-<div class="mt-4 mx-4">
-<span class="text-lg font-serif font-bold">Most Popular</span>
-</div>
-            
-
-            <div class="flex w-full gap-3 px-4 py-4">
-
-                <div class="flex justify-center items-center">
-                    <img src="/content/blog1.jpg" alt="" class="w-25 h-25 object-cover rounded-lg shrink-0">
-                </div>
-
-                <div class="flex flex-col justify-between items-start min-w-0">
-                    <div class="flex gap-1 items-center">
-                        <img src="/icons/code.svg" alt="" class="w-5 h-5">
-                        <span class="text-xs font-serif font-bold italic">Laravel 11</span>
-                    </div>
-
-                    <h3 class="text-lg font-serif font-bold text-gray-900 leading-snug">
-                        Sit officia eiusmod mollit aliqua ea ipsum cillum sit.
-                    </h3>
-                    
-                    <div class="flex gap-3">
-                        <span class="text-xs font-light font-serif text-gray-300">
-                            Code
-                        </span>
-                        <span class="text-xs font-light font-serif text-gray-300">
-                            14 Feb 2026
-                        </span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flex w-full gap-3 px-4 py-4">
-
-                <div class="flex justify-center items-center">
-                    <img src="/content/blog1.jpg" alt="" class="w-25 h-25 object-cover rounded-lg shrink-0">
-                </div>
-
-                <div class="flex flex-col justify-between items-start min-w-0">
-                    <div class="flex gap-1 items-center">
-                        <img src="/icons/code.svg" alt="" class="w-5 h-5">
-                        <span class="text-xs font-serif font-bold italic">Laravel 11</span>
-                    </div>
-
-                    <h3 class="text-lg font-serif font-bold text-gray-900 leading-snug">
-                        Sit officia eiusmod mollit aliqua ea ipsum cillum sit.
-                    </h3>
-                    
-                    <div class="flex gap-3">
-                        <span class="text-xs font-light font-serif text-gray-300">
-                            Code
-                        </span>
-                        <span class="text-xs font-light font-serif text-gray-300">
-                            14 Feb 2026
-                        </span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flex w-full gap-3 px-4 py-4">
-
-                <div class="flex justify-center items-center">
-                    <img src="/content/blog1.jpg" alt="" class="w-25 h-25 object-cover rounded-lg shrink-0">
-                </div>
-
-                <div class="flex flex-col justify-between items-start min-w-0">
-                    <div class="flex gap-1 items-center">
-                        <img src="/icons/code.svg" alt="" class="w-5 h-5">
-                        <span class="text-xs font-serif font-bold italic">Laravel 11</span>
-                    </div>
-
-                    <h3 class="text-lg font-serif font-bold text-gray-900 leading-snug">
-                        Sit officia eiusmod mollit aliqua ea ipsum cillum sit.
-                    </h3>
-                    
-                    <div class="flex gap-3">
-                        <span class="text-xs font-light font-serif text-gray-300">
-                            Code
-                        </span>
-                        <span class="text-xs font-light font-serif text-gray-300">
-                            14 Feb 2026
-                        </span>
-                    </div>
+<section class="w-full">
+    <div class="flex max-lg:flex-col gap-4 justify-center mt-4 mx-6 items-center">
+        <div class="flex max-md:flex-col items-center gap-2 h-auto w-auto ">
+            <img src="/content/blog1.jpg" alt="" class="h-32 max-md:h-auto rounded-xl max-md:rounded-sm">
+            <div class="flex flex-col gap-1 justify-start items-start py-2">
+                <h1 class="text-judul-2 line-clamp-1">Lorem, ipsum dolor.</h1>
+                <p class=" line-clamp-2 text-body">Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit.
+                    Voluptate quis voluptates totam, optio sequi dolores!</p>
+                <button class="text-blue-600 font-semibold mt-2 hover:text-blue-800 focus:outline-none">
+                    Read more
+                </button>
+                <div class="flex">
+                    <span class="text-sm font-sans text-gray-300">Tech</span>.
+                    <span class=" flex text-sm font-sans text-gray-300">20 Nov 2026</span>
                 </div>
             </div>
         </div>
+
+        <div class="flex max-md:flex-col items-center gap-2 h-auto w-auto ">
+            <img src="/content/blog1.jpg" alt="" class="h-32 max-md:h-auto rounded-xl max-md:rounded-sm">
+            <div class="flex flex-col gap-1 justify-start items-start py-2">
+                <h1 class="text-judul-2 line-clamp-1">Lorem, ipsum dolor.</h1>
+                <p class=" line-clamp-2 text-body">Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit.
+                    Voluptate quis voluptates totam, optio sequi dolores!</p>
+                <button class="text-blue-600 font-semibold mt-2 hover:text-blue-800 focus:outline-none">
+                    Read more
+                </button>
+                <div class="flex">
+                    <span class="text-sm font-sans text-gray-300">Tech</span>.
+                    <span class=" flex text-sm font-sans text-gray-300">20 Nov 2026</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex max-md:flex-col items-center gap-2 h-auto w-auto ">
+            <img src="/content/blog1.jpg" alt="" class="h-32 max-md:h-auto rounded-xl max-md:rounded-sm">
+            <div class="flex flex-col gap-1 justify-start items-start py-2">
+                <h1 class="text-judul-2 line-clamp-1">Lorem, ipsum dolor.</h1>
+                <p class=" line-clamp-2 text-body">Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit.
+                    Voluptate quis voluptates totam, optio sequi dolores!</p>
+                <button class="text-blue-600 font-semibold mt-2 hover:text-blue-800 focus:outline-none">
+                    Read more
+                </button>
+                <div class="flex">
+                    <span class="text-sm font-sans text-gray-300">Tech</span>.
+                    <span class=" flex text-sm font-sans text-gray-300">20 Nov 2026</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="flex flex-row max-lg:flex-col xl:justify-center gap-4 m-4">
+        <div class="flex basis-[60%] bg-gray-200 border rounded-2xl h-125">
+            <img src="/content/content1.jpg" alt="" class="w-full h-full object-cover rounded-2xl">
+        </div>
+        <div class="flex basis-[40%] bg-gray-300 border rounded-2xl h-auto"></div>
+
     </div>
 </section>
