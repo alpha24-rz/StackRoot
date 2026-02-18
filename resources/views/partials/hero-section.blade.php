a5506faf451d7932cf68bb0696b49e5cf3117717
Muhammad alfarizi <alfakiddrock7@gmail.com>
2026-02-18 14:34:24 +0800

    update hero section 2

--- resources/views/partials/hero-section.blade.php
+++ resources/views/partials/hero-section.blade.php
@@ -1,63 +1,312 @@
 <section class="w-full">
-    <div class="flex max-lg:flex-col gap-4 justify-center mt-4 mx-6 items-center">
-        <div class="flex max-md:flex-col items-center gap-2 h-auto w-auto ">
-            <img src="/content/blog1.jpg" alt="" class="h-32 max-md:h-auto rounded-xl max-md:rounded-sm">
-            <div class="flex flex-col gap-1 justify-start items-start py-2">
-                <h1 class="text-judul-2 line-clamp-1">Lorem, ipsum dolor.</h1>
-                <p class=" line-clamp-2 text-body">Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit.
-                    Voluptate quis voluptates totam, optio sequi dolores!</p>
-                <button class="text-blue-600 font-semibold mt-2 hover:text-blue-800 focus:outline-none">
-                    Read more
-                </button>
-                <div class="flex">
-                    <span class="text-sm font-sans text-gray-300">Tech</span>.
-                    <span class=" flex text-sm font-sans text-gray-300">20 Nov 2026</span>
-                </div>
-            </div>
-        </div>
+    <div class="flex max-lg:flex-col gap-8 m-4">
 
-        <div class="flex max-md:flex-col items-center gap-2 h-auto w-auto ">
-            <img src="/content/blog1.jpg" alt="" class="h-32 max-md:h-auto rounded-xl max-md:rounded-sm">
-            <div class="flex flex-col gap-1 justify-start items-start py-2">
-                <h1 class="text-judul-2 line-clamp-1">Lorem, ipsum dolor.</h1>
-                <p class=" line-clamp-2 text-body">Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit.
-                    Voluptate quis voluptates totam, optio sequi dolores!</p>
-                <button class="text-blue-600 font-semibold mt-2 hover:text-blue-800 focus:outline-none">
-                    Read more
-                </button>
-                <div class="flex">
-                    <span class="text-sm font-sans text-gray-300">Tech</span>.
-                    <span class=" flex text-sm font-sans text-gray-300">20 Nov 2026</span>
-                </div>
-            </div>
-        </div>
+        <!-- CARD -->
+        <div class="flex flex-row items-stretch gap-4 w-full ">
 
-        <div class="flex max-md:flex-col items-center gap-2 h-auto w-auto ">
-            <img src="/content/blog1.jpg" alt="" class="h-32 max-md:h-auto rounded-xl max-md:rounded-sm">
-            <div class="flex flex-col gap-1 justify-start items-start py-2">
-                <h1 class="text-judul-2 line-clamp-1">Lorem, ipsum dolor.</h1>
-                <p class=" line-clamp-2 text-body">Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit.
-                    Voluptate quis voluptates totam, optio sequi dolores!</p>
-                <button class="text-blue-600 font-semibold mt-2 hover:text-blue-800 focus:outline-none">
-                    Read more
-                </button>
-                <div class="flex">
-                    <span class="text-sm font-sans text-gray-300">Tech</span>.
-                    <span class=" flex text-sm font-sans text-gray-300">20 Nov 2026</span>
-                </div>
+            <!-- IMAGE -->
+            <div class="w-24 max-sm:w-15 shrink-0 overflow-hidden max-md:rounded-l-xl lg:rounded-xl">
+                <img src="/content/blog1.jpg" class="w-auto h-full object-cover" alt="">
             </div>
+
+            <!-- CONTENT -->
+            <div class="flex flex-col flex-1 justify-between">
+
+                <div>
+                    <h1 class="text-h4 max-md:text-sm  font-bold line-clamp-1">
+                        Lorem, ipsum dolor.
+                    </h1>
+
+                    <p class="text-sm text-gray-500 line-clamp-2 max-md:line-clamp-1">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Voluptate quis voluptates totam
+                    </p>
+                </div>
+
+                <div class="flex items-center justify-between mt-2">
+
+                    <button class="text-sm max-md:text-xs  text-blue-500 hover:text-blue-600">
+                        Read more
+                    </button>
+
+                    <div class="text-xs text-gray-400 flex gap-2">
+                        <span>Tech</span>
+                        <span>•</span>
+                        <span>20 Nov 2026</span>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        <div class="flex flex-row items-stretch gap-4 w-full ">
+
+            <!-- IMAGE -->
+            <div class="w-24 max-sm:w-15 shrink-0 overflow-hidden max-md:rounded-l-xl lg:rounded-xl">
+                <img src="/content/blog1.jpg" class="w-auto h-full object-cover" alt="">
+            </div>
+
+            <!-- CONTENT -->
+            <div class="flex flex-col flex-1 justify-between">
+
+                <div>
+                    <h1 class="text-h4 max-md:text-sm  font-bold line-clamp-1">
+                        Lorem, ipsum dolor.
+                    </h1>
+
+                    <p class="text-sm text-gray-500 line-clamp-2 max-md:line-clamp-1">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Voluptate quis voluptates totam
+                    </p>
+                </div>
+
+                <div class="flex items-center justify-between mt-2">
+
+                    <button class="text-sm max-md:text-xs  text-blue-500 hover:text-blue-600">
+                        Read more
+                    </button>
+
+                    <div class="text-xs text-gray-400 flex gap-2">
+                        <span>Tech</span>
+                        <span>•</span>
+                        <span>20 Nov 2026</span>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        <div class="flex flex-row items-stretch gap-4 w-full ">
+
+            <!-- IMAGE -->
+            <div class="w-24 max-sm:w-15 shrink-0 overflow-hidden max-md:rounded-l-xl lg:rounded-xl">
+                <img src="/content/blog1.jpg" class="w-auto h-full object-cover" alt="">
+            </div>
+
+            <!-- CONTENT -->
+            <div class="flex flex-col flex-1 justify-between">
+
+                <div>
+                    <h1 class="text-h4 max-md:text-sm  font-bold line-clamp-1">
+                        Lorem, ipsum dolor.
+                    </h1>
+
+                    <p class="text-sm text-gray-500 line-clamp-2 max-md:line-clamp-1">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Voluptate quis voluptates totam
+                    </p>
+                </div>
+
+                <div class="flex items-center justify-between mt-2">
+
+                    <button class="text-sm max-md:text-xs  text-blue-500 hover:text-blue-600">
+                        Read more
+                    </button>
+
+                    <div class="text-xs text-gray-400 flex gap-2">
+                        <span>Tech</span>
+                        <span>•</span>
+                        <span>20 Nov 2026</span>
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
 
     </div>
 
-    <div class="flex flex-row max-lg:flex-col xl:justify-center gap-4 m-4">
-        <div class="flex basis-[60%] bg-gray-200 border rounded-2xl h-125">
+    <div class="flex flex-row max-lg:flex-col xl:justify-center gap-4 mx-6 my-8">
+
+        <div class="basis-[60%] bg-gray-100  border rounded-2xl h-125">
             <img src="/content/content1.jpg" alt="" class="w-full h-full object-cover rounded-2xl">
         </div>
-        <div class="flex basis-[40%] bg-gray-300 border rounded-2xl h-auto"></div>
+
+        <div class="basis-[40%] bg-white shadow-md rounded-2xl h-auto">
+            <div class="flex flex-col gap-2 m-4">
+
+                {{-- Category --}}
+                <div class="flex justify-start items-start">
+                    <h1 class="text-h4">Most Recent</h1>
+                </div>
+
+                {{-- Container Card --}}
+                <div class="flex flex-col gap-4 items-stretch">
+
+                    {{-- Card --}}
+                    <div class="flex gap-4">
+
+                        <div class="w-20 overflow-hidden shrink-0 rounded-md">
+                            <img src="/content/blog1.jpg" alt="" class=" h-full object-cover">
+                        </div>
+
+
+                        <div class="flex flex-col gap-1">
+                            <div class="flex justify-start items-center gap-1">
+                                <img src="/icons/code.svg" alt="" class="w-5 h-5 max-md:w-4 max-md:h-4">
+                                <span class="text-body">Tech</span>
+                            </div>
+
+                            <h1 class="text-xl max-md:text-lg font-bold ">Lorem ipsum dolor sit amet.</h1>
+                            <span class="text-body">20 Nov 2026</span>
+                        </div>
+
+                    </div>
+                    {{-- Card --}}
+                    <div class="flex gap-4">
+
+                        <div class="w-20 overflow-hidden shrink-0 rounded-md">
+                            <img src="/content/blog1.jpg" alt="" class=" h-full object-cover">
+                        </div>
+
+
+                        <div class="flex flex-col gap-1">
+                            <div class="flex justify-start items-center gap-1">
+                                <img src="/icons/code.svg" alt="" class="w-5 h-5 max-md:w-4 max-md:h-4">
+                                <span class="text-body">Tech</span>
+                            </div>
+
+                            <h1 class="text-xl max-md:text-lg font-bold ">Lorem ipsum dolor sit amet.</h1>
+                            <span class="text-body">20 Nov 2026</span>
+                        </div>
+
+                    </div>
+                    {{-- Card --}}
+                    <div class="flex gap-4">
+
+                        <div class="w-20 overflow-hidden shrink-0 rounded-md">
+                            <img src="/content/blog1.jpg" alt="" class=" h-full object-cover">
+                        </div>
+
+
+                        <div class="flex flex-col gap-1">
+                            <div class="flex justify-start items-center gap-1">
+                                <img src="/icons/code.svg" alt="" class="w-5 h-5 max-md:w-4 max-md:h-4">
+                                <span class="text-body">Tech</span>
+                            </div>
+
+                            <h1 class="text-xl max-md:text-lg font-bold ">Lorem ipsum dolor sit amet.</h1>
+                            <span class="text-body">20 Nov 2026</span>
+                        </div>
+
+                    </div>
+                    {{-- Card --}}
+                    <div class="flex gap-4">
+
+                        <div class="w-20 overflow-hidden shrink-0 rounded-md">
+                            <img src="/content/blog1.jpg" alt="" class=" h-full object-cover">
+                        </div>
+
+
+                        <div class="flex flex-col gap-1">
+                            <div class="flex justify-start items-center gap-1">
+                                <img src="/icons/code.svg" alt="" class="w-5 h-5 max-md:w-4 max-md:h-4">
+                                <span class="text-body">Tech</span>
+                            </div>
+
+                            <h1 class="text-xl max-md:text-lg font-bold ">Lorem ipsum dolor sit amet.</h1>
+                            <span class="text-body">20 Nov 2026</span>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
 
     </div>
+
+    <div class="flex flex-row w-full h-auto my-12">
+
+        {{-- Upcomming Events --}}
+
+        <div class="basis-[40%] pr-12 py-8 pl-6 border-r w-auto h-auto">
+            <div class="flex flex-col w-full h-auto gap-2 ">
+                <div class="w-full justify-center items-center">
+                    <h1 class="text-h4">Upcoming Events</h1>
+                </div>
+
+                {{-- Container Card --}}
+                <div class="w-auto h-auto gap-4">
+
+                    {{-- card --}}
+                    <div class="flex flex-col justify-between items-start py-6 gap-2 border-b">
+                        <div class="flex text-xl font-medium text-[#A51C30]">
+                            <span>Today</span>
+                            <span>- Feb. 16, 2026</span>
+                        </div>
+
+                        <div class="flex">
+                            <h1 class="text-2xl font-medium text-black">Lorem ipsum dolor sit amet consectetur.</h1>
+                        </div>
+
+                        <div class="flex text-sm text-black justify-center items-center">
+                            <span class="text-date">12 a.m. Sunday</span>
+                            <div class="w-1 h-1 bg-[#A51C30] m-1 "></div>
+                            <span class="text-date">Harvard Dance Center, 66 Garden St. , Cambridge</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-between items-start py-6 gap-2 border-b">
+                        <div class="flex text-xl font-medium text-[#A51C30]">
+                            <span>Today</span>
+                            <span>- Feb. 16, 2026</span>
+                        </div>
+
+                        <div class="flex">
+                            <h1 class="text-2xl font-medium text-black">Lorem ipsum dolor sit amet consectetur.</h1>
+                        </div>
+
+                        <div class="flex text-sm text-black justify-center items-center">
+                            <span class="text-date">12 a.m. Sunday</span>
+                            <div class="w-1 h-1 bg-[#A51C30] m-1 "></div>
+                            <span class="text-date">Harvard Dance Center, 66 Garden St. , Cambridge</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-between items-start py-6 gap-2 border-b">
+                        <div class="flex text-xl font-medium text-[#A51C30]">
+                            <span>Today</span>
+                            <span>- Feb. 16, 2026</span>
+                        </div>
+
+                        <div class="flex">
+                            <h1 class="text-2xl font-medium text-black">Lorem ipsum dolor sit amet consectetur.</h1>
+                        </div>
+
+                        <div class="flex text-sm text-black justify-center items-center">
+                            <span class="text-date">12 a.m. Sunday</span>
+                            <div class="w-1 h-1 bg-[#A51C30] m-1 "></div>
+                            <span class="text-date">Harvard Dance Center, 66 Garden St. , Cambridge</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        {{-- Highlight --}}
+
+        <div class="basis-[60%] pl-12 py-8 pr-6">
+            <div class="flex flex-col gap-4">
+                <div class="h-120 w-full overflow-hidden flex justify-center items-center">
+                    <img src="/content/content2.jpg" alt="" class="w-full object-cover ">
+                </div>
+                <div class="flex flex-col justify-start items-start">
+                    <span class="text-sm text-black">Vibe Coding</span>
+
+                    <h1 class="text-xl font-medium text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem!</h1>
+                    <p class="line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sit doloribus ipsam.</p>
+
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </section>
